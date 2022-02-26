ef45739dfce22456bc818ecca1fcc16a494c069d
Rufus Marigi <77732676+RufusMarigi@users.noreply.github.com>
2022-02-26 21:20:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,25 @@ if (close) {
         nav.classList.remove('active');
     });
 }
+
+
+// PRODUCT PAGE SCRIPT//
+
+var mainImage = document.getElementById("mainImage");
+var smallImage = document.getElementsByClassName("small-image");
+
+smallImage[0].onclick = function() {
+    mainImage.src = smallImage[0].src;
+};
+
+smallImage[1].onclick = function() {
+    mainImage.src = smallImage[1].src;
+};
+
+smallImage[2].onclick = function() {
+    mainImage.src = smallImage[2].src;
+};
+
+smallImage[3].onclick = function() {
+    mainImage.src = smallImage[3].src;
+};
