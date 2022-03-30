925ff15d36738f8ee835f170126ff228b6652a6a
Rufus Marigi <77732676+RufusMarigi@users.noreply.github.com>
2022-03-30 15:04:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -38,97 +38,24 @@ smallImage[3].onclick = function() {
 
 
 
-//MODAL ITEMS
-const modal = document.getElementById('email-modal');
-const openButton = document.querySelector('.main-button');
-const closeButton = document.querySelector('.close-button');
 
-//CLICK EVENTS
-openButton.addEventListener('click', () => {
-    modal.style.display = 'block';
-});
 
-closeButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-});
 
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.style.display = 'none';
-    }
-});
-
-//Form Validation
-
-const form = document.getElementById('form');
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const passwordConfirm = document.getElementById('password-confirm');
-
-//Show Error Message
-
-function showError(input, message) {
-    const formValidation = input.parentElement;
-    formValidation.className = 'form-validation error';
-
-    const errorMessage = formValidation.querySelector('p');
-    errorMessage.innerText = message;
+// ADD TO CART
+let add_to_cart_btns = document.getElementsByClassName('addToCart');
+for (let i = 0; i < add_to_cart_btns.length; i++) {
+    add_to_cart_btns[i].addEventListener('click', addToCart);
 }
 
-//Show Valid Message
+function addToCart(event) {
 
-function showValid(input) {
-    const formValidation = input.parentElement;
-    formValidation.className = 'form-validation valid';
+    let btn = event.target;
+    let btn_parent = btn.parentElement;
+    let btn_grandparent = btn.parentElement.parentElement;
+    let itemName = btn_parent.children[0].innerText;
+    //let itemPrice =
+    let itemImage =
+
+        console.log(itemName);
+
 }
-
-//Check Required Fields
-
-function checkRequired(inputArr) {
-    inputArr.forEach(function(input) {
-        if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
-        } else {
-            showValid(input);
-        }
-    });
-}
-
-//Check Input Length
-
-function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-    } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-    } else {
-        showValid(input);
-    }
-}
-
-//Check Password Matches Password
-
-function passwordMatch(input1, input2) {
-    if (input1.value !== input2.value) {
-        showError(input2, 'Password must match');
-    }
-}
-
-//Get FieldName
-
-function getFieldName(input) {
-    return input.name.charAt(0).toUpperCase() + input.name.slice(1);
-}
-
-//Event Listeners
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    checkRequired([name, email, password, passwordConfirm]);
-    checkLength(name, 3, 30);
-    checkLength(password, 8, 25);
-    checkLength(passwordConfirm, 8, 25);
-    passwordMatch(password, passwordConfirm);
-});
